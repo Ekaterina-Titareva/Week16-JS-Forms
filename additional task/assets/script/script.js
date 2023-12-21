5173eae4380651cbe75f56142a791e7d7ce16ed6
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2023-12-21 23:41:48 +0300

    finished the additional task

--- additional task/assets/script/script.js
+++ additional task/assets/script/script.js
@@ -37,10 +37,13 @@ const carOwners = form.elements.carOwners;
 const mileage = form.elements.mileage;
 const classNameUsed = document.querySelector(".used");
 const total = document.querySelector(".total");
+const paymentMethodText = document.querySelector(".paymentMethodText");
+const reset = document.querySelector(".reset");
+const errorEngineCapacity = document.getElementById('errorEngineCapacity');
+const errorMileage = document.getElementById('errorMileage');
 
 
 const makesOfCar = ["Mazda", "Lada", "BMW", "Mercedes", "Audi"];
-
 
 
 const modelsOfMazda = ["CX-4", "CX-5", "MX-30", "3", "5"];
@@ -57,6 +60,10 @@ const pricesOfAudi = [55000, 45000, 40000, 55000, 35000];
 const modelsOfCar = [modelsOfMazda, modelsOfLada, modelsOfBMW, modelsOfMercedes, modelsOfAudi];
 const pricesOfCar = [pricesOfMazda, pricesOfLada, pricesOfBMW, pricesOfMercedes, pricesOfAudi];
 
+function validateEngineCapacity(engineCapacity) {
+    const regex = /^[1-3]\.?[0-9]?/gm;
+    return regex.test(engineCapacity);
+};
 
 
 function removeElementsByClass(elementsClassName){
@@ -64,16 +71,17 @@ function removeElementsByClass(elementsClassName){
     while(elements.length > 0){
         elements[0].parentNode.removeChild(elements[0]);
     }
-}
+};
 
 
 function addOptions(select, array, className) {
 for (let index = 0; index < array.length; index++) {
 	select.innerHTML += `<option ${className} value="${index}">${array[index]}</option>`;
 }
-}
+};
 
 addOptions(selectMakes, makesOfCar);
+
 
 selectMakes.addEventListener("change", function(value){
 	const classNameModel = "class = 'model'";
@@ -81,25 +89,22 @@ selectMakes.addEventListener("change", function(value){
 
 	let index = selectMakes.value;
 	addOptions(selectModels, modelsOfCar[index], classNameModel);
-}
-)
-
-const ifConditionUsed = document.querySelector(".ifConditionUsed");
+});
 
 
 conditionOfCar[1].addEventListener("change", function(evt){
 if (evt.target.value === "used") {
 	classNameUsed.classList.remove("used");
+	conditionOfCar[0].disabled = true;
 }
-})
+});
 
 conditionOfCar[0].addEventListener("change", function(evt){
 	if (evt.target.value === "new") {
 		classNameUsed.classList.add("used");
+		conditionOfCar[1].disabled = true;
 	}
-})
-
-
+});
 
 
 let engineCapacityValue;
@@ -109,80 +114,108 @@ let pricesListOfCar;
 
 
 form.addEventListener("change", function calculatePrice(event) {
+	errorEngineCapacity.style.display = 'none';
+	errorMileage.style.display = 'none';
 
 if (event.target.name === "makesOfCar") {
     let makeIndex = event.target.value;
-    // console.log(makeIndex);
     pricesListOfCar = pricesOfCar[makeIndex];
-    // console.log("pricesListOfCar");
-    // console.log(pricesListOfCar);
-    // console.log(event.target);
 	priceOfCar = '';
-}
+};
+
 if (event.target.name === "modelsOfCar") {
     let modelIndex = event.target.value;
-    // console.log(modelIndex);
-    // console.log(pricesListOfCar);
-    // console.log(pricesListOfCar[modelIndex]);
 	priceOfCar = pricesListOfCar[modelIndex];
-}
+};
 
 if (event.target.name === "typeOfFuel" ) {
-	// console.log(event.target.checked);
 	if (event.target.checked === true) { priceOfCar += parseFloat(event.target.value); 
-		console.log(priceOfCar);
-		return priceOfCar;
+		// console.log(priceOfCar);
 	} else if (event.target.checked === false) {priceOfCar -= parseFloat(event.target.value);
-		console.log(priceOfCar);
-		return priceOfCar;}
+		// console.log(priceOfCar);
+		}
 	}
 
 if (event.target.name === "engineCapacity") {
-    console.log(`Engine Capacity, L: ${event.target.value}`);
+    // console.log(`Engine Capacity, L: ${event.target.value}`);
 	if (event.target.value !== ''){
 	engineCapacityValue = event.target.value;
+		if (validateEngineCapacity(form.elements.engineCapacity.value) === false) {
+		errorEngineCapacity.textContent = "Enter the correct your car's engine capacity (from 1 to 3). Please use a dot to separate decimal numbers.";
+		errorEngineCapacity.style.display = 'block';
+		};
+	
 	priceOfCar += parseFloat(engineCapacityValue * 100);
-	console.log(priceOfCar);
-        engineCapacity.disabled = true;
-		}
-    // } else if (event.target.value === ''){engineCapacity.disabled = false;}
-}
+	// console.log(priceOfCar);
+    engineCapacity.disabled = true;
+	}
+};
 
 
 if (event.target.name === "conditionOfCar") {
-    console.log(`Condition of your Car: ${event.target.value}`);
+    // console.log(`Condition of your Car: ${event.target.value}`);
+	// console.log(form.elements.engineCapacity.value);
+	if (form.elements.engineCapacity.value === "1.6")
+	{priceOfCar += 16*100}
 	if (event.target.value === 'used'){
-		priceOfCar = priceOfCar*0.7;
+		engineCapacity.disabled = true;
 	}
 }
 
 if (event.target.name === "carOwners") {
-	console.log(`The number of car owners: ${event.target.value}`);
-	console.log(event.target.checked);
+	// console.log(`The number of car owners: ${event.target.value}`);
+	// console.log(event.target.checked);
 	if (event.target.checked === true) { priceOfCar -= parseFloat(event.target.value); 
-		console.log(priceOfCar);
+		// console.log(priceOfCar);
 		carOwners[0].disabled = true;
-		return priceOfCar;
+		carOwners[1].disabled = true;
 	} else if (event.target.checked === false) {priceOfCar += parseFloat(event.target.value);
-		console.log(priceOfCar);
+		// console.log(priceOfCar);
 		carOwners[0].disabled = true;
-		return priceOfCar;}
+		carOwners[1].disabled = true;
+	}
 }
 
-
-if (event.target.name === "paymentMethod") {
-    console.log(`Payment Method: ${event.target.value}`);
-}
+if (event.target.name === "carOwners") {
+	// console.log(`The number of car owners: ${event.target.value}`);
+	// console.log(event.target.checked);
+	if (event.target.checked === true) { priceOfCar -= parseFloat(event.target.value); 
+		// console.log(priceOfCar);
+		carOwners[0].disabled = true;
+	} else if (event.target.checked === false) {priceOfCar += parseFloat(event.target.value);
+		// console.log(priceOfCar);
+		carOwners[0].disabled = true;
+	}
+};
 
 
 if (event.target.name === "mileage") {
-    console.log(`Mileage, km: ${event.target.value}`);
-	priceOfCar = priceOfCar - event.target.value*0.1;
-	console.log(priceOfCar);
-	mileage.disabled = true;
+	if (event.target.value >= 0 && event.target.value <= 150000) {
+		priceOfCar = priceOfCar - event.target.value*0.1;
+		mileage.disabled = true;
+	} else { 
+		errorMileage.textContent = "Enter the correct mileage (from 0 to 150000).";
+		errorMileage.style.display = 'block';
+		mileage.disabled = true;
+	}
 	
-}
+};
+
+if (event.target.name === "paymentMethod") {
+    // console.log(`Payment Method: ${event.target.value}`);
+	if (event.target.value === "payByCard")
+	{paymentMethodText.textContent = `The payment method you have chosen is payment by card`
+} else if (event.target.value === "payInCash")
+{paymentMethodText.textContent = `The payment method you have chosen is payment in cash`
+} else if (event.target.value === "invoice")
+{paymentMethodText.textContent = `The payment method you have chosen is invoice for a legal person`}
+};
 
 total.textContent = `Your car rate is about ${priceOfCar.toLocaleString('en-US')}\$`
 
 });
+
+
+reset.addEventListener ("click", function(){
+	form.reset();
+} )
